8af892f33762020347c7522d2c6986d4fb8c05d1
DevRedFA <Sedykhivan@gmail.com>
2017-12-17 14:07:15 +0300

    Corrected APIs and method names for uniformity.

--- src/main/java/com/epam/controller/NoteController.java
+++ src/main/java/com/epam/controller/NoteController.java
@@ -3,9 +3,11 @@ package com.epam.controller;
 import com.epam.models.Note;
 import com.epam.models.Notebook;
 import com.epam.models.Tag;
+import com.epam.models.User;
 import com.epam.services.interfaces.NoteService;
 import com.epam.services.interfaces.NotebookService;
 import com.epam.services.interfaces.TagService;
+import com.epam.services.interfaces.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -30,6 +32,32 @@ public class NoteController {
     @Autowired
     private TagService tagService;
 
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/{userId}", method = RequestMethod.GET)
+    public String getAllNotesFromUser(@PathVariable long userId, ModelMap model) {
+        Set<Tag> tags = userService.getById(userId).getTags();
+        //TODO: return tags to model
+        return "user";
+    }
+
+    @RequestMapping(value = "/{userId}/tag/{tagId}", method = RequestMethod.GET)
+    public String getAllNotesFromUserWithTag(@PathVariable long userId, @PathVariable long tagId,
+                                             ModelMap model) {
+        List<Note> resultNotes = new ArrayList<>();
+        Tag tag = tagService.getById(tagId);
+        User user = userService.getById(userId);
+        for (Note note : user.getNotes()) {
+            if (note.getTags().contains(tag)) {
+                resultNotes.add(note);
+            }
+        }
+        //TODO: upload resultNotes instance to model
+        //TODO: refactor noteService method to remove logic from that controller
+        return "user";
+    }
+
     @RequestMapping(value = "/{userId}/{notebookId}", method = RequestMethod.GET)
     public String getAllNotesFromNotebook(@PathVariable long userId, @PathVariable long notebookId,
                                           ModelMap model) {
@@ -37,24 +65,6 @@ public class NoteController {
         Set<Note> notes = notebook.getNotes();
         //TODO: upload notes instance to model
         //TODO: refactor noteService method for removing notebookService from that controller
-        return "user";
-    }
-
-    @RequestMapping(value = "/{userId}/{notebookId}/tag/{tagId}", method = RequestMethod.GET)
-    public String getAllNotesWithTagFromNotebook(@PathVariable long userId,
-                                                 @PathVariable long notebookId,
-                                                 @PathVariable long tagId, ModelMap model) {
-        List<Note> result = new ArrayList<>();
-        Notebook notebook = notebookService.getById(notebookId);
-        Set<Note> notes = notebook.getNotes();
-        Tag tagToFilter = tagService.getById(tagId);
-        for (Note note : notes) {
-            if (note.getTags().contains(tagToFilter)) {
-                result.add(note);
-            }
-        }
-        //TODO: upload notes instance to model
-        //TODO: refactor noteService method for removing notebookService and tagService from that controller
         return "user";
     }
 
@@ -66,15 +76,6 @@ public class NoteController {
         Notebook notebook = notebookService.getById(notebookId);
         notebook.getNotes().add(note);
         notebookService.update(notebook);
-        return "user";
-    }
-
-    @RequestMapping(value = "/", method = RequestMethod.PUT)
-    public String createNote(@PathVariable long userId, @PathVariable long notebookId,
-                             ModelMap model) {
-        Note note = new Note();
-        //TODO: set parameter from model to note instance
-        noteService.save(note);
         return "user";
     }
 
@@ -101,5 +102,24 @@ public class NoteController {
         noteService.delete(noteId);
         return "user";
     }
+
+    @RequestMapping(value = "/{userId}/{notebookId}/tag/{tagId}", method = RequestMethod.GET)
+    public String getAllNotesFromNotebookWithTag(@PathVariable long userId,
+                                                 @PathVariable long notebookId,
+                                                 @PathVariable long tagId, ModelMap model) {
+        List<Note> result = new ArrayList<>();
+        Notebook notebook = notebookService.getById(notebookId);
+        Set<Note> notes = notebook.getNotes();
+        Tag tagToFilter = tagService.getById(tagId);
+        for (Note note : notes) {
+            if (note.getTags().contains(tagToFilter)) {
+                result.add(note);
+            }
+        }
+        //TODO: upload notes instance to model
+        //TODO: refactor noteService method for removing notebookService and tagService from that controller
+        return "user";
+    }
+
 
 }
