732d7be4570fda0d19f6ed3fa9d54b86bef6127d
DevRedFA <Sedykhivan@gmail.com>
2017-12-19 00:17:02 +0300

    Updated all with new service methods.

--- src/main/java/com/epam/service/models/Tag.java
+++ src/main/java/com/epam/service/models/Tag.java
@@ -1,14 +1,19 @@
 package com.epam.service.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 @Data
 @RequiredArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Tag {
 
+    @JsonIgnore
     private long id;
+
     @NonNull
     private String name;
 }
