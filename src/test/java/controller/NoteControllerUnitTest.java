9a8435a69b661e026afde6f5e9b0cd9c5338fcee
Alina <alina_golovanova@epam.com>
2017-12-22 11:04:11 +0300

    added noteControllerUnitTests and tagControllerUnitTests

---
+++ src/test/java/controller/NoteControllerUnitTest.java
@@ -0,0 +1,110 @@
+package controller;
+
+
+import com.epam.controller.NoteController;
+import com.epam.dto.NoteDto;
+import com.epam.dto.interfaces.NoteDtoService;
+import com.epam.mapper.NoteMapper;
+import com.epam.service.models.Note;
+import com.epam.service.models.Notebook;
+import com.epam.service.models.Tag;
+import com.epam.service.models.User;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+public class NoteControllerUnitTest {
+
+    @Mock
+    private NoteDtoService noteService;
+
+    @InjectMocks
+    private NoteController noteController;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        mvc = MockMvcBuilders
+                .standaloneSetup(noteController)
+                .build();
+    }
+
+
+    @Test
+    public void testGetByUserId() throws Exception {
+        mvc.perform(get("/noteDtos/0"))
+                .andExpect(status().isOk());
+        verify(noteService, times(1)).getByUserId(0);
+    }
+
+    @Test
+    public void testGetByUserAndTagId() throws Exception {
+        mvc.perform(get("/noteDtos/0/tag/0"))
+                .andExpect(status().isOk());
+        verify(noteService, times(1)).getByUserIdAndTagId(0, 0);
+    }
+
+    @Test
+    public void testGetTestByUserAndNotebookAndNoteId() throws Exception {
+        mvc.perform(get("/noteDtos/0/0/0"))
+                .andExpect(status().isOk());
+        verify(noteService, times(1)).getById(0);
+    }
+
+    @Test
+    public void testGetTestByUserAndNotebookAndTagId() throws Exception {
+        mvc.perform(get("/noteDtos/0/0/tag/0"))
+                .andExpect(status().isOk());
+        verify(noteService, times(1)).getByNotebookIdAndTagId(0, 0);
+    }
+
+
+    @Test
+    public void testPut() throws Exception {
+        User user = new User(1, "Ivan1", "password", new HashSet<Tag>(), new HashSet<Notebook>(), new HashSet<Note>());
+        Note note = new Note(3, "note", "text", user, new Notebook(),
+                new HashSet<>(), null, null);
+        NoteDto noteDto = NoteMapper.toNoteDto(note);
+        String newNote = "{\"name\":\"note\",\"text\":\"text\",\"tags\":[]}";
+        mvc.perform(put("/noteDtos/1/3")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(newNote))
+                .andExpect(status().isOk());
+        verify(noteService, times(1)).save(3, noteDto);
+    }
+
+    @Test
+    public void testPost() throws Exception {
+        User user = new User(1, "Ivan1", "password", new HashSet<Tag>(), new HashSet<Notebook>(), new HashSet<Note>());
+        Note note = new Note(3, "note", "text", user, new Notebook(),
+                new HashSet<>(), null, null);
+        NoteDto noteDto = NoteMapper.toNoteDto(note);
+        String newNote = "{\"name\":\"note\",\"text\":\"text\",\"tags\":[]}";
+        mvc.perform(post("/noteDtos/0/0/0")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(newNote))
+                .andExpect(status().isOk());
+        verify(noteService, times(1)).update(0, noteDto);
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        mvc.perform(delete("/noteDtos/0/0/0"))
+                .andExpect(status().isOk());
+        doNothing().when(noteService).delete(0);
+    }
+}
---
+++ src/test/java/controller/TagControllerUnitTest.java
@@ -0,0 +1,93 @@
+package controller;
+
+
+import com.epam.controller.TagController;
+import com.epam.dto.TagDto;
+import com.epam.dto.interfaces.TagDtoService;
+import com.epam.mapper.TagMapper;
+import com.epam.service.models.Tag;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class TagControllerUnitTest {
+
+
+    @Mock
+    private TagDtoService tagService;
+
+    @InjectMocks
+    private TagController tagController;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        mvc = MockMvcBuilders
+                .standaloneSetup(tagController)
+                .build();
+    }
+
+    @Test
+    public void testGet() throws Exception {
+        mvc.perform(get("/tags/0"))
+                .andExpect(status().isOk());
+        verify(tagService, times(1)).getByUserId(0);
+    }
+
+    @Test
+    public void testGetByUserNotebookNoteId() throws Exception {
+        mvc.perform(get("/tags/0/0/0"))
+                .andExpect(status().isOk());
+        verify(tagService, times(1)).getByNoteId(0);
+    }
+
+    @Ignore
+    @Test
+    public void testPut() throws Exception {
+        Tag tag = new Tag("new tag");
+        TagDto tagDto = TagMapper.toTagDto(tag);
+        when(tagService.save(0, tagDto)).thenReturn(tagDto);
+        String newTag = "[{\"name\":\"new tag\"}]";
+        mvc.perform(put("/tags")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(newTag))
+                .andExpect(status().isOk());
+        verify(tagService, times(0)).save(0, tagDto);
+    }
+
+    @Ignore
+    @Test
+    public void testPost() throws Exception {
+        Tag tag = new Tag("new tag");
+        TagDto tagDto = TagMapper.toTagDto(tag);
+        when(tagService.save(0, tagDto)).thenReturn(tagDto);
+        String newTag = "[{\"name\":\"new tag\"}]";
+        mvc.perform(post("/tags/0/0")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(newTag))
+                .andExpect(status().isOk());
+        verify(tagService, times(0)).save(0, tagDto);
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        Tag tag = new Tag("new tag");
+        TagDto tagDto = TagMapper.toTagDto(tag);
+        doNothing().when(tagService).delete(0);
+        mvc.perform(delete("/tags/0/0"))
+                .andExpect(status().isOk());
+        verify(tagService, times(1)).delete(0);
+    }
+}
